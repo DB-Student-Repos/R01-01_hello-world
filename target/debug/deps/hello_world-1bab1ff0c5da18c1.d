0fa925dc538211e17d06028e170aef4b95a6035a
Boves556 <enechukwulc@gmail.com>
2024-03-16 07:10:16 +0100

    solved

---
+++ target/debug/deps/hello_world-1bab1ff0c5da18c1.d
@@ -0,0 +1,5 @@
+C:\Users\enech\Documents\GitHub\r01-01-hello-world-Boves556\target\debug\deps\libhello_world-1bab1ff0c5da18c1.rmeta: src\lib.rs
+
+C:\Users\enech\Documents\GitHub\r01-01-hello-world-Boves556\target\debug\deps\hello_world-1bab1ff0c5da18c1.d: src\lib.rs
+
+src\lib.rs:
